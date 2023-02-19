725b24c98b17085f3b1728d694d2c08ad44e4cb2
KaMatsubara <106413649+KaMatsubara@users.noreply.github.com>
2023-02-19 21:48:38 +0900

    Add files via upload

---
+++ src/main/java/levywalk/Main.java
@@ -0,0 +1,63 @@
+package levywalk;
+
+import java.io.*;
+import java.util.Random;
+
+
+public class Main {
+    public static void main(String[] args) {
+        String[] arguments = fileRead(args[0]); // 入力ファイルを解析
+        if (arguments == null) {
+            System.out.println("Failed to parse the file.");
+            return;
+        }
+
+        OptionCli options = new OptionCli(); // オプションを定義
+        Data d = new Data();
+        options.parse(arguments); // 引数を解析
+        options.setArguments(d); // 引数をセット
+
+        
+        while(d.targetThreshold > d.threshold){
+            while(d.step <= 1600){
+                options.setFilename(d);
+                 for (Integer i = 0; i < d.trial; i++) {
+                    d.graphSeed = new Random().nextLong();
+                    d.current_trial = i;
+                    RandomWalkOnGraph rwGraph = new RandomWalkOnGraph(d);
+                    rwGraph.run();
+                }
+                d.step = d.step * 2;
+                
+            }
+            d.step = 100;
+            d.threshold += d.intarvalsOfThreshold;
+        }
+    
+        System.out.println("\noutput the result to \"" + d.file.getName() + "\"");
+        return;
+    }
+
+    /* データファイルを解析 */
+    private static String[] fileRead(String fileName) {
+        File file = new File(fileName);
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String str = "";
+            String line;
+            while ((line = br.readLine()) != null)
+                str += line + " ";
+            br.close();
+
+            String[] strs = str.split(" ");
+            for (int i = 0; i < strs.length; i += 2)
+                strs[i] = "-" + strs[i];
+            return strs;
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/levywalk/OptionCli.java
@@ -0,0 +1,125 @@
+package levywalk;
+
+import org.apache.commons.cli.*;
+import java.io.File;
+
+public class OptionCli {
+    Options options = new Options();
+    CommandLine cmdln;
+
+    OptionCli() {
+        /* オプションを定義 */
+        options.addOption("trial", true, "number of trials");
+        options.addOption("node", true, "number of nodes");
+        options.addOption("threshold", true, "value of threshold");
+        options.addOption("targetThreshold", true, "value of target threshold");
+        options.addOption("intarvalsOfThreshold", true, "intarval of sampling");
+        options.addOption("researchCoverRatio", true, "researchCoverRatio");
+        options.addOption("entityClass", true, "kind of entity");
+        options.addOption("step", true, "number of steps");
+        options.addOption("entity", true, "number of entitys");
+        options.addOption("remake", true, "upper limit of remake");
+        options.addOption("file", true, "file name");
+        options.addOption("lambda", true, "value of lambda");
+        options.addOption("alpha", true, "value of alpha");
+        options.addOption("gamma", true, "value of gamma");
+        options.addOption("permissibleError", true, "value of permissibleError");
+        options.addOption("graphSeed", true, "randomSeed to create Graph");
+        options.addOption("walkSeed", true, "randomSeed to walk");
+        options.addOption("interval", true, "value of interval");
+        options.addOption("debug", true, "for debug");
+    }
+
+    /* 解析 */
+    public void parse(String[] args) {
+        CommandLineParser line = new DefaultParser();
+        try {
+            cmdln = line.parse(options, args);
+        } catch (Exception exp) {
+            System.err.println("ERROR: " + exp.getMessage());
+        }
+        return;
+    }
+
+    /* オプションの値をセットする */
+    public void setArguments(Data d) {
+        String str;
+        if ((str = cmdln.getOptionValue("trial")) != null)
+            d.trial = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("node")) != null)
+            d.node = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("threshold")) != null)
+            d.threshold = Double.parseDouble(str);
+
+        if ((str = cmdln.getOptionValue("targetThreshold")) != null)
+            d.targetThreshold = Double.parseDouble(str);
+
+        if ((str = cmdln.getOptionValue("intarvalsOfThreshold")) != null)
+            d.intarvalsOfThreshold = Double.parseDouble(str);
+
+        if ((str = cmdln.getOptionValue("researchCoverRatio")) != null)
+            d.researchCoverRatio = Boolean.valueOf(str);
+
+        if ((str = cmdln.getOptionValue("entityClass")) != null)
+            d.entityClass = str;
+        // d.entityClass = str.class.getName();
+
+        if ((str = cmdln.getOptionValue("step")) != null)
+            d.step = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("entity")) != null)
+            d.entity = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("remake")) != null)
+            d.remake = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("file")) != null) {
+            str = "result/" + str;
+            d.file = new File((str.endsWith(".csv")) ? str : str.concat(".csv"));
+        }
+
+        if ((str = cmdln.getOptionValue("lambda")) != null) {
+            String[] strs = str.split(",");
+            for (int i = 0; i < d.entity; i++) {
+                if (strs.length <= i) {
+                    d.lambda[i] = 1.2;
+                } else {
+                    d.lambda[i] = Double.parseDouble(strs[i]);
+                }
+            }
+        }
+
+        if ((str = cmdln.getOptionValue("alpha")) != null)
+            d.alpha = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("gamma")) != null)
+            d.gamma = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("permissibleError")) != null)
+            d.permissibleError = Double.parseDouble(str);
+
+        if ((str = cmdln.getOptionValue("graphSeed")) != null)
+            d.graphSeed = Long.parseLong(str);
+
+        if ((str = cmdln.getOptionValue("walkSeed")) != null)
+            d.walkSeed = Long.parseLong(str);
+
+        if ((str = cmdln.getOptionValue("interval")) != null)
+            d.interval = Integer.parseInt(str);
+
+        if ((str = cmdln.getOptionValue("debug")) != null)
+            d.debug = Boolean.valueOf(str);
+
+        return;
+    }
+    public void setFilename(Data d) {
+        String str;
+        Double th = Math.round(d.threshold * 100.0)/100.0; //小数点第三位で四捨五入
+
+        str = "result/" + d.entityClass + "_" + String.valueOf(d.entity) + "_" + String.valueOf(th) + "_" + String.valueOf(d.step);
+        d.file = new File((str.endsWith(".csv")) ? str : str.concat(".csv"));
+    }
+    
+}
